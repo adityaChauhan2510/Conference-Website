1b111e3d5d626939678a6090990ae4835e8dfc81
Samarth Namdeo <namdeosamarth@gmail.com>
2024-03-07 18:42:34 +0530

    Fix attempt

--- client/src/components/KeynoteSpeaker.js
+++ client/src/components/KeynoteSpeaker.js
@@ -6,19 +6,21 @@ export default function KeynoteSpeaker() {
   return (
     <>
       <Heading text="Chief Guest" />
-      <div className="flex justify-center items-center bg-gray-200 p-4 rounded-lg">
-        <img alt="" style={{ width:'8cm' }} src="https://faculty.dese.iisc.ac.in/kgopakumar/wp-content/uploads/sites/7/2017/03/kgopa3.jpg" />
-        <a href="https://faculty.dese.iisc.ac.in/kgopakumar/">
-          <div className="ml-4">
-            <h1 className="text-lg font-bold">
-              Professor<br/>
-              Dr. K. Gopakumar<br/>
-              Department of Electronic Systems Engineering (Formerly CEDT)<br/>
-              Indian Institute of Science (IISc)<br/>
-              Bangalore 560012
-            </h1>
+      <div className="flex flex-col items-center bg-gray-200 p-4 rounded-lg">
+        <div className="flex flex-col items-center">
+          <img alt="" style={{ width: '8cm' }} src="https://faculty.dese.iisc.ac.in/kgopakumar/wp-content/uploads/sites/7/2017/03/kgopa3.jpg" />
+          <div className="mt-4">
+            <a href="https://faculty.dese.iisc.ac.in/kgopakumar/">
+              <h1 className="text-lg font-bold text-center">
+                Professor<br />
+                Dr. K. Gopakumar<br />
+                Department of Electronic Systems Engineering (Formerly CEDT)<br />
+                Indian Institute of Science (IISc)<br />
+                Bangalore 560012
+              </h1>
+            </a>
           </div>
-        </a>
+        </div>
       </div>
     </>
   )
